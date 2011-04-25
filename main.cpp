21414190fb58818a7f0101cb8a8bbb360cdaf9b3
Mirnoth <tim.rasputin@gmx.de>
2011-04-25 22:17:26 +0200

    basic view changing

--- main.cpp
+++ main.cpp
@@ -35,6 +35,29 @@ int main() {
 	Polygon.Move(300, 300);
 	Polygon.Scale(3, 2);
 	Polygon.Rotate(45);
+	// Create a sprite for the background
+	sf::Image BackgroundImage;
+	if (!BackgroundImage.LoadFromFile("background.jpg"))
+		return EXIT_FAILURE;
+	sf::Sprite Background(BackgroundImage);
+	// Create another sprite for the cursor
+	sf::Image CursorImage;
+	if (!CursorImage.LoadFromFile("cursor.tga"))
+		return EXIT_FAILURE;
+	sf::Sprite Cursor(CursorImage);
+	// Resize the background, so that it's much bigger than the window
+	Background.Resize(2000, 2000);
+	// Define a text for displaying some instructions
+	sf::String Text("Use the arrow keys to move the camera\nUse the + and - keys to zoom and unzoom");
+	Text.Move(10, 500);
+	Text.SetColor(sf::Color::White);
+	// Create a view with the same size as the window, located somewhere in the center of the background
+	sf::Vector2f Center(1000, 1000);
+	sf::Vector2f HalfSize(400, 300);
+	sf::View View(Center, HalfSize);
+	// To start, put the cursor at the center
+	Cursor.SetCenter(32, 32);
+	Cursor.SetPosition(Center);
 	while (App.IsOpened()) {
 		sf::Event Event;
 		while (App.GetEvent(Event)) {
@@ -84,10 +107,33 @@ int main() {
 			if (App.GetInput().IsKeyDown(sf::Key::Subtract)) Polygon.Rotate(+ 100 * ElapsedTime);
 			App.Draw(Polygon);
 		} else {
+			// Move the view using arrow keys
+			float Offset = 200.f * App.GetFrameTime();
+			if (App.GetInput().IsKeyDown(sf::Key::Up))    View.Move( 0,      -Offset);
+			if (App.GetInput().IsKeyDown(sf::Key::Down))  View.Move( 0,       Offset);
+			if (App.GetInput().IsKeyDown(sf::Key::Left))  View.Move(-Offset,  0);
+			if (App.GetInput().IsKeyDown(sf::Key::Right)) View.Move( Offset,  0);
+			// Zoom and unzoom using + and - keys
+			if (App.GetInput().IsKeyDown(sf::Key::Add))      View.Zoom(1.010f);
+			if (App.GetInput().IsKeyDown(sf::Key::Subtract)) View.Zoom(0.990f);
+			// Set the view
+			App.SetView(View);
+			// Clear screen
+			App.Clear();
+			// Draw background
+			App.Draw(Background);
 			// Draw predefined shapes
 			App.Draw(sf::Shape::Line(10, 10, 710, 100, 15, sf::Color::Red));
 			App.Draw(sf::Shape::Circle(200, 200, 100, sf::Color::Yellow, 10, sf::Color::Blue));
 			App.Draw(sf::Shape::Rectangle(350, 200, 600, 350, sf::Color::Green));
+			// Draw cursor
+			sf::Vector2f CursorPos = App.ConvertCoords(App.GetInput().GetMouseX(), App.GetInput().GetMouseY());
+			Cursor.SetPosition(CursorPos);
+			App.Draw(Cursor);
+			// Reset to the default view to draw the interface
+			App.SetView(App.GetDefaultView());
+			// Draw some instructions
+			App.Draw(Text);
 		}
 		// Display window contents on screen
 		App.Display();
