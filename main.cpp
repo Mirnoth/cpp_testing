c2aa0c695142204c21761df097949128e8c42ff3
Mirnoth <tim.rasputin@gmx.de>
2011-04-25 17:26:42 +0200

    SFML Basics..

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+int main() {
+	sf::RenderWindow App(sf::VideoMode(800, 600, 32), "SFML Window");
+	App.SetFramerateLimit(60);
+	while (App.IsOpened()) {
+		sf::Event Event;
+		while (App.GetEvent(Event)) {
+			// Window closed
+			if (Event.Type == sf::Event::Closed)
+				App.Close();
+			// Escape key pressed
+			if ((Event.Type == sf::Event::KeyPressed) && (Event.Key.Code == sf::Key::Escape))
+				App.Close();
+		}
+		// Clear the screen (fill it with black color)
+       App.Clear(sf::Color(200, 200, 0));
+
+        // Display window contents on screen
+        App.Display();
+	}
+	return EXIT_SUCCESS;
 }
